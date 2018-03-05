5998367fdff87baefa1a6c5bb0aefc3cf60e7068
Nguyễn Văn Quyết <vanquyet94@gmail.com>
2018-03-05 11:59:39 +0700

    Update Removing Default Image Link in WordPress.php

--- tutorial/Removing Default Image Link in WordPress.php
+++ tutorial/Removing Default Image Link in WordPress.php
@@ -3,3 +3,9 @@ function theme_unlink_images() {
 	  update_option('image_default_link_type', 'none');
 }
 add_action('after_setup_theme', 'theme_unlink_images');
+
+/************** OR ********************/
+function admin_imagelink_setup() {
+	update_option('image_default_link_type', 'none');
+}
+add_action('admin_init', 'admin_imagelink_setup', 10);
