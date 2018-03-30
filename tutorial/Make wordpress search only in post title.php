a6dd95a1490846e4b7cf1e9d174d404ecdab80d5
Nguyễn Văn Quyết <vanquyet94@gmail.com>
2018-03-30 16:00:24 +0700

    Create Make wordpress search only in post title.php

---
+++ tutorial/Make wordpress search only in post title.php
@@ -0,0 +1,23 @@
+<?php
+function __search_by_title_only( $search, &$wp_query ){
+    global $wpdb;
+    if(empty($search)) {
+        return $search; // skip processing - no search term in query
+    }
+    $q = $wp_query->query_vars;
+    $n = !empty($q['exact']) ? '' : '%';
+    $search =
+    $searchand = '';
+    foreach ((array)$q['search_terms'] as $term) {
+        $term = esc_sql($wpdb->esc_like($term));
+        $search .= "{$searchand}($wpdb->posts.post_title LIKE '{$n}{$term}{$n}')";
+        $searchand = ' AND ';
+    }
+    if (!empty($search)) {
+        $search = " AND ({$search}) ";
+        if (!is_user_logged_in())
+            $search .= " AND ($wpdb->posts.post_password = '') ";
+    }
+    return $search;
+}
+add_filter('posts_search', '__search_by_title_only', 500, 2);
