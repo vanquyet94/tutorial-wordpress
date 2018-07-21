74bb81a7d8d8bdb4da87377f41c077913dc4abcb
Nguyễn Văn Quyết <vanquyet94@gmail.com>
2018-07-21 17:39:57 +0700

    Create How to remove “Category:” from category title.php

---
+++ tutorial/How to remove “Category:” from category title.php
@@ -0,0 +1,8 @@
+<?php 
+function prefix_category_title( $title ) {
+    if ( is_category() ) {
+        $title = single_cat_title( '', false );
+    }
+    return $title;
+}
+add_filter( 'get_the_archive_title', 'prefix_category_title' );
